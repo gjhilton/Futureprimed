5c743a0699f411883df99624785410c612a1df30
gjhilton <gjh@robocoppelia.com>
2013-09-24 17:34:25 +0100

    timeline play

--- timeline/timeline.ino
+++ timeline/timeline.ino
@@ -8,23 +8,24 @@
 // HARDCODED CUES
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
-#define N_CUES 5
+#define N_CUES 4
 
 void initCues(){
-  setCue(0,1,0);
-  setCue(1,10,10);
-  setCue(2,5,30);
-  setCue(3,60,30);
-  setCue(4,5,0);
+  setCue(0,  20,   5);
+  setCue(1,  5,   30);
+  setCue(2,  30,  30);
+  setCue(3,  5,   0);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // NETWORK CONFIGURATION
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
-byte mac[] = {0x90, 0xA2, 0xDA, 0x0E, 0x9B, 0x7F};
+byte mac[] = {
+  0x90, 0xA2, 0xDA, 0x0E, 0x9B, 0x7F};
 IPAddress ip(192, 168, 1, 12);
 EthernetServer server(80);
+SoftwareSerial mySerial(16,17);
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 // GLOBALS
@@ -32,11 +33,9 @@ EthernetServer server(80);
 
 int cueSeconds[N_CUES];
 int cueValues[N_CUES];
-
 boolean running;
 int currentCue, currentSpeed;
 unsigned long nextCueTime;
-SoftwareSerial mySerial(16,17);
 // SoftEasyTransfer ET; 
 OSCServer listener;
 
@@ -49,25 +48,16 @@ void setup() {
   goWait();
   Ethernet.begin(mac, ip);
   server.begin();
-  
-  Serial.begin(9600);
-  //Serial.print("server is at ");
-  //Serial.println(Ethernet.localIP());
-}
 
+  Serial.begin(9600);
+}
 
 void loop() {
   webserver();
   if (running){
     unsigned long timeNow = millis();
     if (timeNow >= nextCueTime) {
-      currentCue++;
-      if (currentCue > N_CUES){
-        // timeline has finished
-        goWait();
-      } else {
-        nextCueTime = timeNow + (cueSeconds[currentCue] * 1000);
-      }
+      goToCue(currentCue +1); 
     }
   }
 }
@@ -76,17 +66,40 @@ void loop() {
 // STATE
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
+void setMotorSpeed(int speed){
+  Serial.print("setting speed to ");
+  Serial.print(speed);
+}
+
+void goToCue(int cue){
+  currentCue = cue;
+  if (currentCue >= N_CUES){
+    goWait(); // timeline has finished
+  } 
+  else {
+    unsigned long now = millis();
+    nextCueTime = now + (cueSeconds[currentCue] * 1000);
+    Serial.print("Beginning cue:");
+    Serial.print(currentCue);
+    Serial.print(" at ");
+    Serial.print(now);
+    Serial.print(" Duration: ");
+    Serial.print(cueSeconds[currentCue]);
+    Serial.print(" seconds. Next cue at: ");
+    Serial.println(nextCueTime);
+  }
+}
+
 void goRun(){
+  Serial.println("run");
   running = true;
-  currentCue = 0;
-  nextCueTime = millis() + (cueSeconds[0] * 1000);
-  Serial.print("run");
+  goToCue(0);
 }
 
 void goWait(){
   running = false;
   currentSpeed = 0;
-  Serial.print("wait");
+  Serial.println("wait");
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
@@ -102,45 +115,39 @@ void setCue(int index, int seconds, int value){
 // STATUS REPORTING
 ////////////////////////////////////////////////////////////////////////////////////////////////
 
+void serialStatus(){
+  Serial.print("Time:");
+  Serial.println(millis());
+}
+
 void webserver(){
   EthernetClient client = server.available();
   if (client) {                             // if you get a client,
-    Serial.println("new client");           // print a message out the serial port
     String currentLine = "";                // make a String to hold incoming data from the client
     while (client.connected()) {            // loop while the client's connected
       if (client.available()) {             // if there's bytes to read from the client,
         char c = client.read();             // read a byte, then
-        // Serial.write(c);                    // print it out the serial monitor
         if (c == '\n') {                    // if the byte is a newline character
-
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
           if (currentLine.length() == 0) {  
             writeStatus(client);
-            // break out of the while loop:
             break;         
           } 
-          else {      // if you got a newline, then clear currentLine:
+          else {
             currentLine = "";
           }
         }     
         else if (c != '\r') {    // if you got anything else but a carriage return character,
           currentLine += c;      // add it to the end of the currentLine
         }
-
-        // Check to see if the client request was "GET /go":
         if (currentLine.endsWith("GET /go")) {
           goRun();               // GET /go runs the timeline
         }
         if (currentLine.endsWith("GET /stop")) {
-          goWait();               // GET /go stops the timeline
+          goWait();               // GET /stop stops the timeline
         }
       }
     }
-    // close the connection:
     client.stop();
-
-    Serial.println("client disonnected");
   }
 }
 
@@ -153,33 +160,33 @@ void writeStatus(EthernetClient client){
   client.println("<html>");
   client.println("<head><meta http-equiv=\"refresh\" content=\"2; url='/'\"><style>body{font-family:helvetica;}td,th{padding:10px;text-align:center;}</style></head>");
   client.println("<body>");
-  
+
   if (!running){
     client.println("<h3>Status: ready</h3>");
     client.print("<p><a href=\"/go\">go</a></p>");
-  } else {
+  } 
+  else {
     client.println("<h3>Status: <span style='color:#0c0'>running</span></h3>");
     client.print("<p><a href=\"/stop\">stop</a></p>");
-    
+    /*
     client.println("<h4>Motor:");
-    client.println(currentSpeed);
-    client.println("</h4>");
-    
-    client.println("<h4>Cue:");
-    client.println(currentCue);
-    client.println("</h4>");
-    
-    client.println("<h4>Time:");
-    client.println(millis());
-    client.println("</h4>");
-    
-    client.println("<h4>Next cue at:");
-    client.println(nextCueTime);
-    client.println("</h4>");
+     client.println(currentSpeed);
+     client.println("</h4>");
+     
+     client.println("<h4>Cue:");
+     client.println(currentCue);
+     client.println("</h4>");
+     
+     client.println("<h4>Time:");
+     client.println(millis());
+     client.println("</h4>");
+     
+     client.println("<h4>Next cue at:");
+     client.println(nextCueTime);
+     client.println("</h4>");
+     */
   }
- 
-  
- 
+
   client.println("<table cellspacing=0 cellpadding=0 style='width:100%; border:1px solid grey;'>");
   client.print("<tr style='background:#eee;'><th>duration</th><th>end speed</th></tr>");
   for (int i= 0; i< N_CUES; i++) {
@@ -194,8 +201,10 @@ void writeStatus(EthernetClient client){
     client.println("</td><tr>"); 
   }
   client.println("</table>");
-       
+
   client.println("</body></html>");
- client.println();
+  client.println();
 }
+
+
 
